62d1ff5cd74bf44bddaa956f0e6ffa8e4a3fab90
Ting-Yen Lai <banacorn@gmail.com>
2013-12-01 19:30:34 +0800

    gadfsdfasdfasdfasdf

---
+++ app/FirstViewController.m
@@ -0,0 +1,63 @@
+//
+//  FirstViewController.m
+//  app
+//
+//  Created by blitz on 2013/12/1.
+//  Copyright (c) 2013年 blitz. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)fetchAndParseDatafrom:(NSString*)urlString {
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSData *rawData = [NSData dataWithContentsOfURL:url];
+    NSString *ret = [[NSString alloc] initWithData:rawData encoding:NSUTF8StringEncoding];
+    NSData* fetched = [ret dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = nil;
+    
+    id object = [NSJSONSerialization
+                 JSONObjectWithData:fetched
+                 options:0
+                 error:&error];
+    
+    if(error) {
+        NSLog(@"JSON malformed");
+    }
+    
+    if([object isKindOfClass:[NSDictionary class]])
+    {
+        NSLog(@"JSON: Object");
+    }
+    else if([object isKindOfClass:[NSArray class]])
+    {
+        NSLog(@"%@", object);
+        data = object;
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // example usage of fetchAndParseDataFrom
+    [self fetchAndParseDataFrom:@"http://banacorn.org:3000/api/board?parent=0"];
+    NSLog(@"ggg %@", data);
+    
+    
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ app/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  app
+//
+//  Created by blitz on 2013/12/1.
+//  Copyright (c) 2013年 blitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
