ee33f12098212ed5ec57618fcdde8c3642219e0d
Ting-Yen Lai <banacorn@gmail.com>
2013-12-24 20:17:12 +0800

    adding new UITableViewController (Timeline)

---
+++ app/Timeline.h
@@ -0,0 +1,13 @@
+//
+//  Timeline.h
+//  app
+//
+//  Created by blitz on 2013/12/24.
+//  Copyright (c) 2013年 blitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Timeline : UITableViewController
+
+@end
