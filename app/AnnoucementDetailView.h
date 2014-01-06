656636da9a10dcdcbc92ebf7bfd227b3e06b01b3
Ting-Yen Lai <banacorn@gmail.com>
2014-01-06 16:10:35 +0800

    AnnouncentDetailView

---
+++ app/AnnoucementDetailView.h
@@ -0,0 +1,15 @@
+//
+//  AnnoucementDetailView.h
+//  app
+//
+//  Created by blitz on 2014/1/6.
+//  Copyright (c) 2014年 blitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnnoucementDetailView : UIWebView
+
+@property (strong, nonatomic) id announcement;
+
+@end
