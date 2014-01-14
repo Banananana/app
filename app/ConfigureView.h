ed84f2ffec759c793374d98aa60632baf7b156d0
Ting-Yen Lai <banacorn@gmail.com>
2014-01-14 14:47:33 +0800

    configure tab

---
+++ app/ConfigureView.h
@@ -0,0 +1,13 @@
+//
+//  ConfigureView.h
+//  app
+//
+//  Created by blitz on 2014/1/14.
+//  Copyright (c) 2014年 blitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigureView : UIViewController
+
+@end
---
+++ app/ConfigureView.m
@@ -0,0 +1,37 @@
+//
+//  ConfigureView.m
+//  app
+//
+//  Created by blitz on 2014/1/14.
+//  Copyright (c) 2014年 blitz. All rights reserved.
+//
+
+#import "ConfigureView.h"
+
+@interface ConfigureView ()
+
+@end
+
+@implementation ConfigureView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
